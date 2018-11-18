79f434c075b83968da842336996a318c7fd97080
Unknown <beatricia.inspirit@gmail.com>
2018-11-18 16:43:33 +0800

    Commit config.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { NgxElectronModule } from 'ngx-electron';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { NavComponent } from './nav/nav.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AngularMaterialModule } from './material-module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    NavComponent
+    NavComponent,
+    PaymentComponent  
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxElectronModule
+    NgxElectronModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    NgbModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {}
