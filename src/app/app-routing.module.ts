61f95fbf2def3032218d4f49ae6d0f56dfeb70b2
Unknown <chloeangsl@gmail.com>
2018-12-21 03:25:43 +0800

    got error don't pull

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,21 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { RegistrationComponent } from './registration/registration.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './guards/auth.guard';
 import { PaymentComponent } from './recep/payment/payment.component';
 import { PatientListComponent } from './recep/patient-list/patient-list.component';
 import { SearchComponent } from './recep/search/search.component';
+import { MainLoginComponent } from './main-login/main-login.component';
+import { DoctorLoginComponent } from './doctor-login/doctor-login.component';
+
 
 const routes: Routes = [
   { path: '',  redirectTo: 'login', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent},
-  { path: 'registration', component: RegistrationComponent},
+  { path: 'main-login', component: MainLoginComponent},
+  { path: 'doctor-login', component: DoctorLoginComponent },
   { path: 'receptionist/payment', component: PaymentComponent },
   { path: 'receptionist/patient-list', component: PatientListComponent },
   { path: 'clinic/registration', component: RegisterComponent, canActivate:[AuthGuard]},
