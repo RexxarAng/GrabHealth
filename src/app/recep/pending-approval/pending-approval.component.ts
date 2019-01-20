0541dcaad8b95372b9c8affa1a6deac630656708
beatricia <beatricia.inspirit@gmail.com>
2019-01-20 20:35:18 +0800

    commit

--- src/app/recep/pending-approval/pending-approval.component.ts
+++ src/app/recep/pending-approval/pending-approval.component.ts
@@ -10,6 +10,19 @@ import { Router } from '@angular/router';
   styleUrls: ['./pending-approval.component.css']
 })
 export class PendingApprovalComponent implements OnInit {
+  patient: any;
+  patientlist: Array<any>;
+  firstName: '';
+  lastName: '';
+  address: '';
+  contactNo: '';
+  nric: '';
+  dob: '';
+  nationality: '';
+  gender: '';
+  email: '';
+  sessionSlot: '';
+
 
   constructor(
     private receptionistService: ReceptionistService,
@@ -19,8 +32,62 @@ export class PendingApprovalComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.getPatients();
   }
 
+
+  // View patient info
+  viewPatientInfo() {
+    let patient = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      address: this.address,
+      nric: this.nric,
+      contactNo: this.contactNo,
+      nationality: this.nationality,
+      dob: this.dob,
+      gender: this.gender,
+      email: this.email
+    }
+    this.patient = patient;
+
+    this.receptionistService.viewPatientInfo(patient).subscribe(
+      res=>{
+        if(res['success']){
+          this.getPatients();
+          this.flashMessagesService.show(res['msg'], { cssClass: 'alert-success', timeout: 3000});
+        } else {
+          if(res['unauthenticated']){
+            this.authService.unAuthenticated();
+            return false;
+          }
+          this.flashMessagesService.show(res['msg'], { cssClass: 'alert-danger', timeout: 3000});
+          this.getPatients();
+        }
+      },
+      err => {
+        this.flashMessagesService.show('Somewhere broke!', { cssClass: 'alert-danger', timeout: 3000});
+      }
+    )    
+
+  }
+
+
+  // Display patients
+  getPatients(){
+    this.receptionistService.getPatients().subscribe(
+      res=>{
+        console.log(res);
+        if(!res['success']){
+          this.flashMessagesService.show(res['msg'], { cssClass: 'alert-danger', timeout: 3000});
+        }        
+        this.patientlist = res['patients'];
+      },
+      err=>{
+      
+      }
+    )
+  }
 
   // Approve Appointment
   onApproveAppointment(){
--- src/app/services/receptionist.service.ts
+++ src/app/services/receptionist.service.ts
@@ -21,8 +21,19 @@ export class ReceptionistService {
       return this.http.post('http://localhost:4560/receptionist/editPatientInfo', patient);
     }
 
-    addPatientToQueue(patient){
+    onAddToQueue(patient){
       return this.http.post('http://localhost:4560/receptionist/addPatientToQueue', patient);
     }
 
+    getQueue(){
+        return this.http.get('http://localhost:4560/receptionist/queue-list');
+    }  
+
+    onRemoveFromQueue(nric){
+      return this.http.post('http://localhost:4560/receptionist/', nric);
+    }
+
+    viewPatientInfo(patient){
+      return this.http.post('http://localhost:4560/receptionist/viewPendingApproval', patient);
+    }
 }
