5d7a3d5f8b5d21bc29639d72455c43539924fcdd
beatricia <beatricia.inspirit@gmail.com>
2019-01-21 01:53:28 +0800

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { NgxElectronModule } from 'ngx-electron';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
 import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AngularMaterialModule } from './material-module';
@@ -47,7 +46,6 @@ export function tokenGetter() {
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent,
     NavComponent,
     RegistrationComponent,
     NavComponent,
